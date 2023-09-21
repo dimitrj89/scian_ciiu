9ddf717bd0f9261819699a9709edd4ff011dbe1c
Dimitrj Bonansea <dimitrj22@gmail.com>
2023-09-20 18:04:12 -0600

    Initial commit

---
+++ run.py
@@ -0,0 +1,11 @@
+from scian_ciiu import (
+    scian_id_to_ciiu,
+    scian_string_to_ciiu,
+    ciiu_id_to_scian,
+    ciiu_string_to_scian
+)
+
+print(scian_id_to_ciiu("111219"))
+print(scian_string_to_ciiu("Cultivo"))
+print(ciiu_id_to_scian("0111"))
+print(ciiu_string_to_scian("Cultivo"))
